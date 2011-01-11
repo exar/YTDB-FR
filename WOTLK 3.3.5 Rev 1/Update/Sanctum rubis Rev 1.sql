09bc73a8b95040016f34e28d3687a4e5b90328a6
mogrande <hugorayman@gmail.com>
2011-01-11 20:48:01 +0100

    Ajout des traductions

---
+++ WOTLK 3.3.5 Rev 1/Update/Sanctum rubis Rev 1.sql
@@ -0,0 +1,19 @@
+SET NAMES latin1;
+
+-- UPDATE --
+-- `locales_creature`
+UPDATE `locales_creature` SET `name_loc2`= 'Halion', `subname_loc2`= 'Le destructeur du Crépuscule' WHERE `entry`= 40142;
+UPDATE `locales_creature` SET `name_loc2`= 'Braise vivante' WHERE `entry`= 40683;
+UPDATE `locales_creature` SET `name_loc2`= 'Inferno vivant' WHERE `entry`= 40681;
+UPDATE `locales_creature` SET `name_loc2`= 'Mande-flamme onyx' WHERE `entry`= 39814;
+UPDATE `locales_creature` SET `name_loc2`= 'Commandant calcinécaille' WHERE `entry`= 40423;
+UPDATE `locales_creature` SET `name_loc2`= 'Calcinécaille d''élite' WHERE `entry`= 40421;
+UPDATE `locales_creature` SET `name_loc2`= 'Agresseur calcinécaille' WHERE `entry`= 40419;
+UPDATE `locales_creature` SET `name_loc2`= 'Invocateur calcinécaille' WHERE `entry`= 40417;
+UPDATE `locales_creature` SET `name_loc2`= 'Gardienne du sanctum Xerestrasza' WHERE `entry`= 40429;
+UPDATE `locales_creature` SET `name_loc2`= 'Pleureuse de Sylvanas' WHERE `entry`= 39048;
+UPDATE `locales_creature` SET `name_loc2`= 'Halion', `subname_loc2`= 'Le destructeur du Crépuscule' WHERE `entry`= 39863;
+UPDATE `locales_creature` SET `name_loc2`= 'Baltharus l''Enfant de la guerre' WHERE `entry`= 39899;
+UPDATE `locales_creature` SET `name_loc2`= 'Baltharus l''Enfant de la guerre' WHERE `entry`= 39751;
+UPDATE `locales_creature` SET `name_loc2`= 'Général Zarithrian' WHERE `entry`= 39746;
+UPDATE `locales_creature` SET `name_loc2`= 'Saviana Ragefeu' WHERE `entry`= 39747;
